1ced5cf448dd281a0010a52f1da3c6b995725a92
It's Vishu <vishalrawat979@gmail.com>
2023-02-24 23:01:48 +0530

    add three view and add some funtionalities!

--- App.js
+++ App.js
@@ -1,10 +1,72 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, TouchableOpacity, Alert } from 'react-native'
+import React, { useState } from 'react'
 
 export default function App() {
+  const [count, setCount] = useState(Number);
+  const increment = () => {
+    setCount(count + 1)
+  }
+
+  const decrement = () => {
+    if (count > 0) {
+      setCount(count - 1)
+    } else {
+      Alert.alert("Bass kar bhai kitna dabaye ga!")
+    }
+  }
+
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <>
+      <View style={style.mainContainer}>
+
+        <TouchableOpacity onPress={() => decrement()}>
+          <View style={style.container}>
+            <Text style={{ fontSize: 50, fontWeight: 'bold' }}>-</Text>
+          </View>
+        </TouchableOpacity>
+
+        <View style={style.outputContainer}>
+          <Text style={style.output}>{count}</Text>
+        </View>
+
+        <TouchableOpacity onPress={() => { increment() }}>
+          <View style={style.container}>
+            <Text style={{ fontSize: 50, fontWeight: 'bold' }}>+</Text>
+          </View>
+        </TouchableOpacity>
+
+      </View>
+    </>
   )
 }
+
+const style = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: "lightblue"
+  },
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'gray',
+    margin: 10,
+    width: 80,
+    height: 80
+  },
+  outputContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 10,
+    width: 120,
+    height: 80
+  },
+  output: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    fontSize: 30,
+    fontWeight: 'bold'
+  }
+})
